ce730da560a9460322a22ac376dc6eb917ebb54c
PavSwap <paavanswaroop2@gmail.com>
2026-07-22 18:59:38 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-const element = document.querySelector("#stylesheet");
-
-const time = new Date().getHours();
-
-if(time >= 2 && time <= 4) {
-    element.href = "style2.css";
-} else {
-    element.href = "style.css";
-}
